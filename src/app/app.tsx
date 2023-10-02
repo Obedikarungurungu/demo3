909871891db2ccdba3f040bdb660ba9103ad3aca
obedi karungurungu <obedi.kar@gmail.com>
2023-10-02 19:20:51 +1100

    Adding bootstrap button and navbar and styling them

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,18 +1,36 @@
+import React, { useState } from "react";
+import './app.css'
 
+const myFunction = () => {
+    return(
+      alert('It is working')
+    );
+}
 
 export function App() {
   return (
+    <>
+        <nav className="navbar bg-body-tertiary"  data-bs-theme="dark">
+      <div className="container-fluid">
+        <a className="navbar-brand"><img src="../public/favicon.ico" alt="" /></a>
+        <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
     <div className='container'>
-      {/* <NxWelcome title="demo3" /> */}
       <div className="box">
         <div className="message">
           <h1>Click To Recive Message</h1>
         </div>
         <div className="alert">
-        <button type="button" className="btn btn-primary">Alert Button</button>
+        <button onClick={myFunction} type="button" className="btn btn-primary" >Alert Button</button>
         </div>
       </div>
     </div>
+
+    </>
   );
 }
 
