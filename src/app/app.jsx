9f42f09c1e4bc00107e030bc89f5f0d81f10d507
obedi karungurungu <obedi.kar@gmail.com>
2023-10-04 01:35:26 +1100

    Finishing up the final code for the weather app

---
+++ src/app/app.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import './app.css'
+import './WeatherApp/WeatherApp'
+import WeatherApp from './WeatherApp/WeatherApp'
+
+export function App() {
+
+
+
+
+  return (
+      <WeatherApp/>
+  );
+}
+
+export default App;
